831138ec7f2f7682c2e58dfb1147990c324136b8
philipjc <philipcox83@gmail.com>
2018-06-08 16:26:08 +0100

    more mobile style. links, breadcrumb.

--- assets/js/routes/common.js
+++ assets/js/routes/common.js
@@ -2,10 +2,75 @@
 class Common {
   constructor() {
 
+    this.readMore = [].slice.call((document.querySelectorAll('.read-more')));
   }
 
+
+  /**
+   *
+   */
   init() {
     console.log('common init');
+
+    this.addEventListeners();
+  }
+
+
+  /**
+   *
+   */
+  addEventListeners() {
+
+    this.readMore.forEach(btn => {
+      btn.addEventListener('click', (e) => {
+        this.increaseCardHeight(e, btn);
+      });
+    });
+
+  }
+
+
+  /**
+   *
+   */
+  increaseCardHeight(e, clickedButton) {
+
+    const targetClasses = e.target.previousSibling.childNodes[3].children[1].classList;
+
+    targetClasses.contains('small')
+      ? this.increaseCard(targetClasses, clickedButton)
+      : this.decreaseCard(targetClasses, clickedButton);
+
+  }
+
+  increaseCard(cardClasses, btn) {
+    cardClasses.remove('small');
+    btn.text = 'Close';
+  }
+
+  decreaseCard(cardClasses, btn) {
+    cardClasses.add('small');
+    btn.text = 'More';
+  }
+
+
+  /**
+   *
+   */
+  removeEventListeners() {
+
+    this.readMore.forEach(btn => {
+      btn.removeEventListener('click', () => {});
+    });
+
+  }
+
+
+  /**
+   *
+   */
+  finalize() {
+    this.removeEventListeners();
   }
 }
 
